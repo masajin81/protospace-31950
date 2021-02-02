4399ae59f7624ac62370dd837dccdd74423bf74c
masajin81 <masajin.0801@gmail.com>
2021-02-02 15:12:33 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:update, :edit, :destroy]
+  before_action :authenticate_user!, only: [:update, :edit, :destroy ,:new]
 
   def index
     @prototypes = Prototype.all
